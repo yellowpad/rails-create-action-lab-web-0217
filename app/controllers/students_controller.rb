620c17e7e6dadb0a601b36ab8ab977c533ddb225
yellowpad <zayneabraham1@gmail.com>
2017-03-01 15:19:53 -0500

    done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,9 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    @student.save
+    redirect_to student_path(@student)
   end
 
 end
